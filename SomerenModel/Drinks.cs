cb7d0fe4d4f7266de9c7d6230c178b5cb88447e0
Wouter <704992@student.inholland.nl>
2023-03-22 11:52:40 +0100

    Drinks added to cash register

---
+++ SomerenModel/Drinks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class Drinks
+    { // moet altijd overeen komen met de database.
+        public int Dranknr { get; set; }
+        public string Naam { get; set; }
+        public decimal Prijs { get; set; }
+        public int Voorraad { get; set; }
+        public int Aantal_Verkocht { get; set; }
+
+
+
+    }
+}
