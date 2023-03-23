b8c0fbaa5dd39843ffa8ad3ac3cbd06f426edb6e
Exp <102148117+Explosive12@users.noreply.github.com>
2023-03-23 17:56:41 +0100

    :)

---
+++ SomerenModel/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    internal class Order
+    {
+        public int Ordernr { get; set; }
+        public string Student { get; set; }
+        public string Drankje { get; set; }
+        public decimal Prijs { get; set; }
+        public DateTime datum { get; set; }
+    }
+}
