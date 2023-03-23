88537b86f867c1b7f90b234a400b3fcff664772f
Cheo-Pog <698719@student.inholland.nl>
2023-03-23 22:47:33 +0100

    revenue v2

--- SomerenDAL/RevenueDAO.cs
+++ SomerenDAL/RevenueDAO.cs
@@ -1,27 +1,21 @@
-﻿using Microsoft.VisualBasic;
-using SomerenModel;
+﻿using SomerenModel;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing.Printing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SomerenDAL
 {
-    
+
     public class RevenueDAO : BaseDao
     {
         private SqlConnection dbConnection;
         string DateFormat2 = "yyyy-MM-dd";
         public Revenue Select(DateTime startDate, DateTime endDate, Revenue revenue)
         {
-            string query = $"select count(ordernr) as sales, sum(prijs) as turnover, count(distinct student) as nCustomers from [order] where datum between '{startDate.ToString(DateFormat2)}' and '{endDate.AddDays(1).ToString(DateFormat2)}'";
+            string query = $"select count(orderid) as sales, sum(price) as turnover, count(distinct studentid) as nCustomers from [orders] where date between '{startDate.ToString(DateFormat2)}' and '{endDate.AddDays(1).ToString(DateFormat2)}'";
             SqlParameter[] sqlParameters = new SqlParameter[0];
-            List<Revenue> revenues= new List<Revenue>();
+            List<Revenue> revenues = new List<Revenue>();
             revenues = ReadTables(ExecuteSelectQuery(query, sqlParameters));
             return revenues[0];
         }
@@ -29,7 +23,8 @@ namespace SomerenDAL
         private List<Revenue> ReadTables(DataTable dataTable)
         {
             List<Revenue> revenues = new List<Revenue>();
-                foreach (DataRow dr in dataTable.Rows)
+            foreach (DataRow dr in dataTable.Rows)
+            {
                 {
                     Revenue revenue = new Revenue()
                     {
@@ -37,10 +32,9 @@ namespace SomerenDAL
                         Turnover = (decimal)dr["turnover"],
                         NumberOfStudents = (int)dr["nCustomers"],
                     };
-
                     revenues.Add(revenue);
                 }
-
+            }
             return revenues;
         }
     }
